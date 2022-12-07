ef81acc5ee6776c43e0f3125e76aa57f2eda786e
Damir <damir.ps04@gmail.com>
2022-12-08 01:37:43 +0600

    sex

--- src/main/java/com/example/spring_mongo_db_sex/controller/MyController.java
+++ src/main/java/com/example/spring_mongo_db_sex/controller/MyController.java
@@ -14,22 +14,6 @@ import java.util.List;
 @AllArgsConstructor
 public class MyController {
     private final StudentService service;
-    @GetMapping("/students")
-    public List<Student> showAllStudents(){
-        return service.getAllStudents();
-    }
-
-    @PostMapping("/addStudent")
-    public Student saveStudent(@RequestBody Student student){
-        student.setCreated(LocalDateTime.now());
-        service.saveStudent(student);
-        return student;
-    }
-    @GetMapping("/students/{id}")
-    public List<Student> getStudentById(@PathVariable String id){
-        List<Student> student = service.getStudentByID(id);
-        return student;
-    }
     @GetMapping("/sex")
     public String helloPage(Model model){
         List<Student> students = service.getAllStudents();
